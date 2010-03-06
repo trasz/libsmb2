4e08d3c4d385d702a26c60364a95e3d0c18e50d5
Edward Tomasz Napierała <trasz@FreeBSD.org>
2010-03-06 14:14:26 +0100

    Multiple include protection.

--- smb2_connection.h
+++ smb2_connection.h
@@ -26,6 +26,9 @@
  * $Id$
  */
 
+#ifndef SMB2_CONNECTION_H
+#define	SMB2_CONNECTION_H
+
 #include <stdint.h>
 
 struct smb2_connection {
@@ -36,3 +39,5 @@ struct smb2_connection {
 
 void				smb2_connection_add_credits(struct smb2_connection *conn, int64_t credits);
 int64_t				smb2_connection_next_message_id(struct smb2_connection *conn);
+
+#endif /* !SMB2_CONNECTION_H */
