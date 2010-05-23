0c92945d0484020f4afdb5dda6192421faf97a53
Edward Tomasz Napierała <trasz@FreeBSD.org>
2010-05-23 15:40:57 +0200

    Improve error reporting.

--- smb2_tcp.c
+++ smb2_tcp.c
@@ -44,7 +44,7 @@ smb2_tcp_send(struct smb2_packet *p)
 	to_write = sizeof(p->p_tcp_encap) + p->p_buf_len;
 	written = write(p->p_conn->c_fd, &p->p_tcp_encap, to_write);
 	if (written != to_write)
-		errx(1, "smb2_tcp_send: write failed; wrote %d, should be %d", written, to_write);
+		err(1, "smb2_tcp_send: write failed; wrote %d, should be %d", written, to_write);
 }
 
 void
@@ -54,13 +54,17 @@ smb2_tcp_receive(struct smb2_packet *p)
 	int32_t encapsulation;
 
 	bytes_read = read(p->p_conn->c_fd, &encapsulation, sizeof(encapsulation));
-	if (bytes_read != sizeof(encapsulation))
-		errx(1, "smb2_tcp_receive: read failed (%d)", bytes_read);
+	if (bytes_read != sizeof(encapsulation)) {
+		if (bytes_read == 0)
+			errx(1, "smb2_tcp_receive: read failed; end of file");
+		else
+			err(1, "smb2_tcp_receive: read failed");
+	}
 	encapsulation = ntohl(encapsulation);
 	if (encapsulation > sizeof(p->p_buf))
 		errx(1, "smb2_tcp_receive: received overlength packet (%d)", encapsulation);
 	bytes_read = read(p->p_conn->c_fd, p->p_buf, encapsulation);
 	if (bytes_read != encapsulation)
-		errx(1, "smb2_tcp_receive: read failed 2");
+		errx(1, "smb2_tcp_receive: read failed; expected %zd bytes, got %zd", encapsulation, bytes_read);
 	p->p_buf_len = encapsulation;
 }
